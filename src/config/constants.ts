e5926272edad0cfffcf55612acb9c66dc18adc4f
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-04 03:17:45 +0200

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,12 @@
-
+export const LOTTERY_ABI = [
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "_initialTicketPriceETH", "type": "uint256"},
+      {"internalType": "address", "name": "_feeWallet", "type": "address"}
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  // ... весь ваш ABI ...
+  {"stateMutability": "payable", "type": "receive"}
+] as const;
