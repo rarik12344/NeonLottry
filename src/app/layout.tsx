5aba4f772f19e1c917b5f0edf34d3256df9540e8
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-04 03:54:32 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,25 @@
-import './styles/globals.css';
+import './globals.css';
 import type { Metadata } from 'next';
+import { Providers } from '@/providers';
 import { SITE_CONFIG } from '@/config/site';
 
 export const metadata: Metadata = {
   title: SITE_CONFIG.name,
   description: SITE_CONFIG.description,
   metadataBase: new URL(SITE_CONFIG.url),
-  openGraph: {
-    images: SITE_CONFIG.frameImage
-  }
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="farcaster"
-          href={`${SITE_CONFIG.url}/.well-known/farcaster.json`}
-          type="application/json"
-        />
-      </head>
       <body className="bg-gray-900 text-white">
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
