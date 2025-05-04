78cd9924073585454250bd31a8779dcf1a1ea9dd
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-04 03:45:15 +0200

    Update web3.ts

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,1 +1,15 @@
+import { createConfig, http } from 'wagmi';
+import { base } from 'wagmi/chains';
+import { injected } from 'wagmi/connectors';
 
+export const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(BASE_RPC_URL),
+  },
+  connectors: [
+    injected({
+      target: 'metaMask',
+    }),
+  ],
+});
