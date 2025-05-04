67d00e2d260ebdb91ec8e67578c7c25f96020e23
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-04 03:19:57 +0200

    Update lottery.ts

--- src/lib/lottery.ts
+++ src/lib/lottery.ts
@@ -1,1 +1,29 @@
+import { createPublicClient, http } from 'viem';
+import { base } from 'viem/chains';
+import { SITE_CONFIG } from '@/config/site';
+import { LOTTERY_ABI } from '@/config/constants';
 
+const client = createPublicClient({
+  chain: base,
+  transport: http()
+});
+
+export async function getCurrentRound() {
+  const [roundInfo, ticketPrice] = await Promise.all([
+    client.readContract({
+      address: SITE_CONFIG.contractAddress,
+      abi: LOTTERY_ABI,
+      functionName: 'getCurrentRoundInfo'
+    }),
+    client.readContract({
+      address: SITE_CONFIG.contractAddress,
+      abi: LOTTERY_ABI,
+      functionName: 'ticketPriceETH'
+    })
+  ]);
+
+  return {
+    endTime: roundInfo.endTime,
+    ticketPrice: ticketPrice
+  };
+}
