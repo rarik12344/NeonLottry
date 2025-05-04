52e84771ec2a2e87ade5c56817d83d2660896412
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-04 03:53:39 +0200

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,16 +3,8 @@ export const CONTRACT_ADDRESS = "0x6927648b3114B8B54FA5476Ec3BC3A52f1ab513B" as 
 export const LOTTERY_ABI = [
   {
     "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_initialTicketPriceETH",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address",
-        "name": "_feeWallet",
-        "type": "address"
-      }
+      {"internalType": "uint256", "name": "_initialTicketPriceETH", "type": "uint256"},
+      {"internalType": "address", "name": "_feeWallet", "type": "address"}
     ],
     "stateMutability": "nonpayable",
     "type": "constructor"
@@ -20,12 +12,7 @@ export const LOTTERY_ABI = [
   {
     "anonymous": false,
     "inputs": [
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "roundIndex",
-        "type": "uint256"
-      }
+      {"indexed": true, "internalType": "uint256", "name": "roundIndex", "type": "uint256"}
     ],
     "name": "RoundCanceled",
     "type": "event"
@@ -33,24 +20,9 @@ export const LOTTERY_ABI = [
   {
     "anonymous": false,
     "inputs": [
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "roundIndex",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "winner",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "prize",
-        "type": "uint256"
-      }
+      {"indexed": true, "internalType": "uint256", "name": "roundIndex", "type": "uint256"},
+      {"indexed": false, "internalType": "address", "name": "winner", "type": "address"},
+      {"indexed": false, "internalType": "uint256", "name": "prize", "type": "uint256"}
     ],
     "name": "RoundEnded",
     "type": "event"
@@ -58,12 +30,7 @@ export const LOTTERY_ABI = [
   {
     "anonymous": false,
     "inputs": [
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "roundIndex",
-        "type": "uint256"
-      }
+      {"indexed": true, "internalType": "uint256", "name": "roundIndex", "type": "uint256"}
     ],
     "name": "RoundStarted",
     "type": "event"
@@ -71,35 +38,16 @@ export const LOTTERY_ABI = [
   {
     "anonymous": false,
     "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "buyer",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "roundIndex",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint32",
-        "name": "tickets",
-        "type": "uint32"
-      }
+      {"indexed": true, "internalType": "address", "name": "buyer", "type": "address"},
+      {"indexed": false, "internalType": "uint256", "name": "roundIndex", "type": "uint256"},
+      {"indexed": false, "internalType": "uint32", "name": "tickets", "type": "uint32"}
     ],
     "name": "TicketsPurchased",
     "type": "event"
   },
   {
     "inputs": [
-      {
-        "internalType": "uint32",
-        "name": "_tickets",
-        "type": "uint32"
-      }
+      {"internalType": "uint32", "name": "_tickets", "type": "uint32"}
     ],
     "name": "buyTickets",
     "outputs": [],
@@ -117,11 +65,7 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "currentRoundIndex",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "stateMutability": "view",
     "type": "function"
@@ -137,11 +81,7 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "feePercent",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "stateMutability": "view",
     "type": "function"
@@ -150,11 +90,7 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "feeWallet",
     "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
+      {"internalType": "address", "name": "", "type": "address"}
     ],
     "stateMutability": "view",
     "type": "function"
@@ -163,85 +99,29 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "getCurrentRoundInfo",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "startTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "endTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "prizePool",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "participantsCount",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "active",
-        "type": "bool"
-      },
-      {
-        "internalType": "bool",
-        "name": "canceled",
-        "type": "bool"
-      },
-      {
-        "internalType": "address",
-        "name": "winner",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "prizeAmount",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "startTime", "type": "uint256"},
+      {"internalType": "uint256", "name": "endTime", "type": "uint256"},
+      {"internalType": "uint256", "name": "prizePool", "type": "uint256"},
+      {"internalType": "uint256", "name": "participantsCount", "type": "uint256"},
+      {"internalType": "bool", "name": "active", "type": "bool"},
+      {"internalType": "bool", "name": "canceled", "type": "bool"},
+      {"internalType": "address", "name": "winner", "type": "address"},
+      {"internalType": "uint256", "name": "prizeAmount", "type": "uint256"}
     ],
     "stateMutability": "view",
     "type": "function"
   },
   {
     "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "roundIndex",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "participantIndex",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "roundIndex", "type": "uint256"},
+      {"internalType": "uint256", "name": "participantIndex", "type": "uint256"}
     ],
     "name": "getParticipantInfo",
     "outputs": [
-      {
-        "internalType": "address",
-        "name": "wallet",
-        "type": "address"
-      },
-      {
-        "internalType": "uint32",
-        "name": "tickets",
-        "type": "uint32"
-      },
-      {
-        "internalType": "uint64",
-        "name": "ticketFrom",
-        "type": "uint64"
-      },
-      {
-        "internalType": "uint64",
-        "name": "ticketTo",
-        "type": "uint64"
-      }
+      {"internalType": "address", "name": "wallet", "type": "address"},
+      {"internalType": "uint32", "name": "tickets", "type": "uint32"},
+      {"internalType": "uint64", "name": "ticketFrom", "type": "uint64"},
+      {"internalType": "uint64", "name": "ticketTo", "type": "uint64"}
     ],
     "stateMutability": "view",
     "type": "function"
@@ -250,11 +130,7 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "owner",
     "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
+      {"internalType": "address", "name": "", "type": "address"}
     ],
     "stateMutability": "view",
     "type": "function"
@@ -263,91 +139,35 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "prizePoolPercent",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "stateMutability": "view",
     "type": "function"
   },
   {
     "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "name": "rounds",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "startTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "endTime",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "isActive",
-        "type": "bool"
-      },
-      {
-        "internalType": "bool",
-        "name": "isCanceled",
-        "type": "bool"
-      },
-      {
-        "internalType": "uint256",
-        "name": "totalTickets",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "prizePool",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address",
-        "name": "winner",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "prizeAmount",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "participantsCount",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bytes32",
-        "name": "randomnessSeed",
-        "type": "bytes32"
-      }
+      {"internalType": "uint256", "name": "startTime", "type": "uint256"},
+      {"internalType": "uint256", "name": "endTime", "type": "uint256"},
+      {"internalType": "bool", "name": "isActive", "type": "bool"},
+      {"internalType": "bool", "name": "isCanceled", "type": "bool"},
+      {"internalType": "uint256", "name": "totalTickets", "type": "uint256"},
+      {"internalType": "uint256", "name": "prizePool", "type": "uint256"},
+      {"internalType": "address", "name": "winner", "type": "address"},
+      {"internalType": "uint256", "name": "prizeAmount", "type": "uint256"},
+      {"internalType": "uint256", "name": "participantsCount", "type": "uint256"},
+      {"internalType": "bytes32", "name": "randomnessSeed", "type": "bytes32"}
     ],
     "stateMutability": "view",
     "type": "function"
   },
   {
     "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_prizePercent",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "_feePercent",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "_prizePercent", "type": "uint256"},
+      {"internalType": "uint256", "name": "_feePercent", "type": "uint256"}
     ],
     "name": "setDistributionPercents",
     "outputs": [],
@@ -356,11 +176,7 @@ export const LOTTERY_ABI = [
   },
   {
     "inputs": [
-      {
-        "internalType": "address",
-        "name": "_newWallet",
-        "type": "address"
-      }
+      {"internalType": "address", "name": "_newWallet", "type": "address"}
     ],
     "name": "setFeeWallet",
     "outputs": [],
@@ -369,11 +185,7 @@ export const LOTTERY_ABI = [
   },
   {
     "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_newPrice",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "_newPrice", "type": "uint256"}
     ],
     "name": "setTicketPrice",
     "outputs": [],
@@ -391,22 +203,14 @@ export const LOTTERY_ABI = [
     "inputs": [],
     "name": "ticketPriceETH",
     "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "stateMutability": "view",
     "type": "function"
   },
   {
     "inputs": [
-      {
-        "internalType": "address",
-        "name": "_newOwner",
-        "type": "address"
-      }
+      {"internalType": "address", "name": "_newOwner", "type": "address"}
     ],
     "name": "transferOwnership",
     "outputs": [],
@@ -419,8 +223,6 @@ export const LOTTERY_ABI = [
   }
 ] as const;
 
-export const DEFAULT_CHAIN_ID = 8453; // Base Mainnet
-export const RPC_URL = "https://mainnet.base.org";
-export const BLOCK_EXPLORER_URL = "https://basescan.org";
+export const BASE_RPC_URL = "https://mainnet.base.org";
+export const BASE_CHAIN_ID = 8453;
 export const TICKET_PRICE_DECIMALS = 18;
-export const MAX_TICKETS_PER_PURCHASE = 100;
