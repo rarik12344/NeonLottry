c343d8d1278fd9ba7c2c5dc27f13e48851a22e24
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-05 23:57:12 +0200

    Update route.ts

--- src/app/(frames)/route.ts
+++ src/app/(frames)/route.ts
@@ -1,27 +1,26 @@
-import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame'
-import { NextResponse } from 'next/server'
-import { NEXT_PUBLIC_URL } from '../../config'
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: Request): Promise<Response> {
-  const body: FrameRequest = await req.json()
-  const { isValid, message } = await getFrameMessage(body, {
-    neynarApiKey: process.env.NEYNAR_API_KEY
-  })
-
-  if (!isValid) {
-    return new NextResponse('Invalid message', { status: 400 })
-  }
+export async function POST(req: NextRequest): Promise<Response> {
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: 'Купить билет',
+      },
+      {
+        label: 'Мои билеты',
+      }
+    ],
+    image: `${process.env.NEXT_PUBLIC_URL}/api/image`,
+    postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame`,
+  });
 
   return new NextResponse(`
     <!DOCTYPE html>
     <html>
       <head>
-        <meta property="fc:frame" content="vNext">
-        <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/api/image">
-        <meta property="fc:frame:button:1" content="Buy Ticket">
-        <meta property="fc:frame:button:2" content="My Tickets">
-        <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame">
+        ${frameMetadata}
       </head>
     </html>
-  `)
+  `);
 }
