84ccb923a0f07f59e3d76c2edc0dadb4fda02909
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-05 23:47:57 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'neonwin.vercel.app'
+        hostname: 'neonwin.vercel.app',
+        pathname: '/**'
       }
     ]
   },
